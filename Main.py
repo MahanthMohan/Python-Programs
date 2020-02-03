af7609f807f15a6914a7bb602b17d119571c8931
Mahanth <mohan.mahanth@gmail.com>
2020-02-03 13:42:34 -0800

    Added Main.py and TestProg.py
    These programs use basic python concepts like format

---
+++ Main.py
@@ -0,0 +1,10 @@
+def getInput():
+    lower_limit = int(input("Enter the lower value"))
+    upper_limit = int(input("Enter the upper value"))
+    array = [lower_limit,upper_limit]
+    print(array)
+    return array
+getInput()
+
+
+
---
+++ TestProg.py
@@ -0,0 +1,2 @@
+a=input("Enter the name of a fruit")
+print('The best fruit is {}'.format(a))
