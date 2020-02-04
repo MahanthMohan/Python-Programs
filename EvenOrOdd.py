34b5990f1aeaf25833e56dc39f3610b0bf8e5466
Mahanth <mohan.mahanth@gmail.com>
2020-02-03 18:21:16 -0800

    Add files via upload
    A program to classify whether the number is even or odd

---
+++ EvenOrOdd.py
@@ -0,0 +1,6 @@
+a = int(input("Enter the dividend "))
+b = 2
+if a%b==0:
+    print("The number {} is divisible by {}".format(a,b))
+else:
+    print("The number {} is not divisible by {}".format(a,b))    
