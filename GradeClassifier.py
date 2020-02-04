2d76a0947671050d9d02cd3c3368efc26920b6e9
Akshat Adsule <akshat.adsule@gmail.com>
2020-02-03 22:42:23 -0800

    Fixed divide by 0 issue, and code cleanup

--- GradeClassifier.py
+++ GradeClassifier.py
@@ -1,17 +1,19 @@
 def gradeClassifier(Score,Total):
-    Percentage = Score/Total*100
-    if Percentage>=90:
-        print("You have an A and your percentage is {}".format(Percentage))
-    elif Percentage>=80:
-        print("You have a B and your percentage is {}".format(Percentage))    
-    elif Percentage>=70:
-        print("You have a C and your percentage is {}".format(Percentage))
-    elif Percentage>=60:
-        print("You have a D and your percentage is {}".format(Percentage))
-    elif Percentage<60:
-        print("You have a F and your percentage is {}".format(Percentage))            
-        print("Failure is the stepping stone to success")
+    if not(Total == 0):
+        Percentage = Score/Total*100
+        if Percentage>=90:
+            return("You have an A and your percentage is {}".format(Percentage))
+        elif Percentage>=80:
+            return("You have a B and your percentage is {}".format(Percentage))    
+        elif Percentage>=70:
+            return("You have a C and your percentage is {}".format(Percentage))
+        elif Percentage>=60:
+            return("You have a D and your percentage is {}".format(Percentage))
+        elif Percentage<60:
+            return("You have a F and your percentage is ", Percentage, " Failure is the stepping stone to success")
+    else:
+        return("Invalid inputs")
 
 Score = int(input("Enter your score: "))
 Total = int(input("Enter the total: "))
-gradeClassifier(Score,Total)
+print (gradeClassifier(Score,Total))
