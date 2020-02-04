a957cbb20b2fd283107c5a722cd631cb69504d4e
Akshat Adsule <akshat.adsule@gmail.com>
2020-02-03 16:52:41 -0800

    Added function doThing; replace doThing code with whatever

--- Main.py
+++ Main.py
@@ -1,10 +1,14 @@
 def getInput():
-    lower_limit = int(input("Enter the lower value"))
-    upper_limit = int(input("Enter the upper value"))
+    lower_limit = int(input("Enter the lower value: "))
+    upper_limit = int(input("Enter the upper value: "))
     array = [lower_limit,upper_limit]
     print(array)
     return array
-getInput()
 
+def doThing(a,b):
+    print (a,b)
+
+array = getInput()
+doThing(array[0],array[1])
 
 
