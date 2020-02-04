326e9f16cc10d0bb17f44d7738c175cd9ca2a750
Akshat Adsule <akshat.adsule@gmail.com>
2020-02-03 18:35:05 -0800

    Update EvenOrOdd.py

--- EvenOrOdd.py
+++ EvenOrOdd.py
@@ -1,6 +1,10 @@
+def evenOrOdd(a,b):
+    if a%b==0:
+        print("The number {} is divisible by {}".format(a,b))
+    else:
+        print("The number {} is not divisible by {}".format(a,b))    
+
+
 a = int(input("Enter the dividend "))
-b = 2
-if a%b==0:
-    print("The number {} is divisible by {}".format(a,b))
-else:
-    print("The number {} is not divisible by {}".format(a,b))    
+b = int(input("Enter the divisor "))
+evenOrOdd(a,b)
