f62b83396b5107dc52ef1fdb7f43e0252c52f581
Akshat Adsule <akshat.adsule@gmail.com>
2020-02-03 22:43:05 -0800

    Fixed divide by 0 issue , and cleaned up code

--- GradeClassifier.py
+++ GradeClassifier.py
@@ -2,13 +2,13 @@ def gradeClassifier(Score,Total):
     if not(Total == 0):
         Percentage = Score/Total*100
         if Percentage>=90:
-            return("You have an A and your percentage is {}".format(Percentage))
+            return("You have an A and your percentage is ", Percentage)
         elif Percentage>=80:
-            return("You have a B and your percentage is {}".format(Percentage))    
+            return("You have an B and your percentage is ", Percentage)
         elif Percentage>=70:
-            return("You have a C and your percentage is {}".format(Percentage))
+            return("You have an C and your percentage is ", Percentage)
         elif Percentage>=60:
-            return("You have a D and your percentage is {}".format(Percentage))
+            return("You have an D and your percentage is ", Percentage)
         elif Percentage<60:
             return("You have a F and your percentage is ", Percentage, " Failure is the stepping stone to success")
     else:
