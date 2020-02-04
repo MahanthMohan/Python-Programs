3c9ca6a434e7e29204de87e1aea5d3c7ef13507c
Akshat Adsule <akshat.adsule@gmail.com>
2020-02-03 17:39:04 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ def getInput():
     lower_limit = int(input("Enter the lower value: "))
     upper_limit = int(input("Enter the upper value: "))
     array = [lower_limit,upper_limit]
-    print(array)
+    #print(array)
     return array
 
 def doThing(a,b):
