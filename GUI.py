c05d34f4842a2e3820365c2109fb0636efb1f09a
Akshat Adsule <akshat.adsule@gmail.com>
2020-02-17 14:41:40 -0800

    support for PyCharm

--- GUI.py
+++ GUI.py
@@ -1,19 +1,20 @@
 import tkinter as tk
+
 h = input("Enter the height ")
 l = input("Enter the length ")
 
 Method = tk.Tk()
 
-canvas = tk.Canvas(Method,height=h,width=l)
+canvas = tk.Canvas(Method, height=h, width=l)
 canvas.pack()
 
-frame = tk.Frame(Method, bg = '#6108cd' )
+frame = tk.Frame(Method, bg='#6108cd')
 frame.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.8)
 
 background_image1 = tk.PhotoImage(file='SouthIndia.png')
-background_label1 = tk.Label(Method, image = background_image1)
-background_label1.place(relwidth = 1.5 , relheight = 1.5)
+background_label1 = tk.Label(Method, image=background_image1)
+background_label1.place(relwidth=1.5, relheight=1.5)
 background_image2 = tk.PhotoImage(file='NorthIndia.png')
-background_label2 = tk.Label(Method, image = background_image2)
-background_label2.place(relwidth = 0.5 , relheight = 0.5)
+background_label2 = tk.Label(Method, image=background_image2)
+background_label2.place(relwidth=0.5, relheight=0.5)
 Method.mainloop()
--- GradeClassifier.py
+++ GradeClassifier.py
@@ -1,19 +1,20 @@
-def gradeClassifier(Score,Total):
-    if not(Total == 0):
-        Percentage = Score/Total*100
-        if Percentage>=90:
-            return("You have an A and your percentage is ", Percentage)
-        elif Percentage>=80:
-            return("You have an B and your percentage is ", Percentage)
-        elif Percentage>=70:
-            return("You have an C and your percentage is ", Percentage)
-        elif Percentage>=60:
-            return("You have an D and your percentage is ", Percentage)
-        elif Percentage<60:
-            return("You have a F and your percentage is ", Percentage, " Failure is the stepping stone to success")
+def grade_classifier(score, total):
+    if not (total == 0):
+        percentage = score / total * 100
+        if percentage >= 90:
+            return "You have an A and your percentage is ", percentage
+        elif percentage >= 80:
+            return "You have an B and your percentage is ", percentage
+        elif percentage >= 70:
+            return "You have an C and your percentage is ", percentage
+        elif percentage >= 60:
+            return "You have an D and your percentage is ", percentage
+        elif percentage < 60:
+            return "You have a F and your percentage is ", percentage, " Failure is the stepping stone to success"
     else:
-        return("Invalid inputs")
+        return "Invalid inputs"
+
 
 Score = int(input("Enter your score: "))
 Total = int(input("Enter the total: "))
-print (gradeClassifier(Score,Total))
+print(grade_classifier(Score, Total))
