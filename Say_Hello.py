3543740571c669f4bb1c886f8a39795d92f74456
Akshat Adsule <akshat.adsule@gmail.com>
2020-02-16 17:52:23 -0800

    s

--- Say_Hello.py
+++ Say_Hello.py
@@ -1,4 +1,4 @@
 def say_hello(first_name,last_name):
-    print("Hello {}{}!".format(first_name,last_name))  
+    print("Hello {} {}!".format(first_name,last_name))  
      
 say_hello(first_name = input("Enter your first name ") , last_name = input("Enter the last name "))   
