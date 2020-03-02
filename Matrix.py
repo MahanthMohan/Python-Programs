54dd317a9c00a8c2f6fb4fb6d39c6384eb4dd2a8
Mahanth <mohan.mahanth@gmail.com>
2020-03-01 21:46:06 -0800

    add files via upload

---
+++ Matrix.py
@@ -0,0 +1,15 @@
+import numpy as Math
+
+def findInv():
+    print(Math.linalg.inv(x))
+
+def findDet():
+    print(Math.linalg.det(x))
+
+def findAdj():
+    print()
+    
+Matrix_length = input(("Enter the length of the matrix"))
+Matrix_width = input(("Enter the width of the matrix"))  
+x = Math.array([Matrix_length],[Matrix_width])           
+print (x) 
