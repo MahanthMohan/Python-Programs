b4c3e83c0ef7fc9a3598cad03ca645e400098e5a
Akshat Adsule <akshat.adsule@gmail.com>
2020-03-02 13:17:38 -0800

    Update Matrix.py

--- Matrix.py
+++ Matrix.py
@@ -9,17 +9,17 @@ def findDet():
 def findAdj():
     print(Math.linalg.adj(x))
 
-row1 = input("Enter the values for row 1 of the matrix)
-row2 = input("Enter the values for row 2 of the matrix)
-row3 = input("Enter the values for row 3 of the matrix)
+row1 = input("Enter the values for row 1 of the matrix")
+row2 = input("Enter the values for row 2 of the matrix")
+row3 = input("Enter the values for row 3 of the matrix")
 x = Math.array([row1],[row2],[row3])           
 print ("This is the matrix that you created " + x) 
 command = input("Enter the operation - Inverse, Determinant, or Adjugate")
-if(command == 'Inverse')
+if(command == 'Inverse'):
    findInv() 
              
-elif(command == 'Determinant')
+elif(command == 'Determinant'):
    findDet() 
 
-elif(command == 'Adjugate')
+elif(command == 'Adjugate'):
    findAdj()              
