7c9aa5484752cd6655a2858bf0a50102e7b2083f
Mahanth <mohan.mahanth@gmail.com>
2020-03-06 19:09:14 -0800

    Update Matrix.py

--- Matrix.py
+++ Matrix.py
@@ -1,53 +1,72 @@
 import numpy as Math # To import numpy package as "Math" for linalg operations with Matrices 
 
+# Methods start 
+             
+# User input for rows and columns
+def MatrixInputM():  
+ R = int(input("Enter the number of rows: ")) 
+ C = int(input("Enter the number of columns: ")) 
+ print("Enter the entries in a single line, rowwise , and spaced out using commas ") 
+ # single line separated by space 
+ enter_values = list(map(float, input().split(","))) # Logic - seperates the float values by taking ',' as a reference, and the map operator accepts values until all rows and columns are maxed out
+ # For printing the matrix 
+ M = Math.matrix(enter_values).reshape(R, C)  # "Reshapes" it into a matrix
+ print("This is your original Matrix")
+ print(M)
+ 
+def MatrixInputX():
+  print("Enter the values for the second matrix")  
+  R = int(input("Enter the number of rows: ")) 
+  C = int(input("Enter the number of columns: ")) 
+  print("Enter the entries in a single line, rowwise , and spaced out using commas ") 
+  # single line separated by space 
+  enter_values = list(map(float, input().split(","))) # Logic - seperates the float values by taking ',' as a reference, and the map operator accepts values until all rows and columns are maxed out
+  # For printing the matrix 
+  X = Math.matrix(enter_values).reshape(R, C)  # "Reshapes" it into a matrix
+  print("This is the second matrix")
+  print(X)
+
 def BasicOps(): # Basic Operations such as add, subtarct, multiply, and divide
     bcommand = input("Enter an operation - add, subtract, multiply, and divide ") # Takes user input for the command
     if(bcommand == "add"): # add
-     X = Math.matrix(enter_values).reshape(R, C)
-     M = Math.linalg.solve(M + X)
-     print(M)
+     MatrixInputM()
+     MatrixInputX()
+     result = Math.linalg.solve(M + X)
+     print(result)
     
     elif(bcommand == "subtract"): # subtract
-     X = Math.matrix(enter_values).reshape(R, C)
-     M = Math.linalg.solve(M - X)
-     print(M)   
+     MatrixInputM()
+     MatrixInputX()
+     result = Math.linalg.solve(M - X)
+     print(result)   
     
     elif(bcommand == "multiply"): # multiply
-     X = Math.matrix(enter_values).reshape(R, C)
-     M = Math.linalg.solve(M * X)
-     print(M)  
+     MatrixInputM()
+     MatrixInputX()
+     result = Math.linalg.solve(M * X)
+     print(result)  
      
     elif(bcommand == "divide"):  # divide
-     X = Math.matrix(enter_values).reshape(R, C)
-     M = Math.linalg.solve(M / X)
-     print(M) 
+     MatrixInputM()
+     MatrixInputX()
+     result = Math.linalg.solve(M / X) 
+     print(result) 
       
 def LinearAlg():
     bcommand = input("Enter an operation - Inverse or Determinant ") # Takes user input for the command
     if(bcommand == "Inverse"): # Inverse
-     M = Math.linalg.inv(M)
+     MatrixInputM()    
+     result = Math.linalg.inv(M)
      print(M)
      
     elif(bcommand == "Determinant"): # Determinant
-     M = Math.linalg.det(M)
+     MatrixInputM()   
+     result = Math.linalg.det(M)
      print(M)
-              
-# User input for rows and columns  
-R = int(input("Enter the number of rows: ")) 
-C = int(input("Enter the number of columns: ")) 
-  
-  
-print("Enter the entries in a single line, rowwise , and spaced out using commas ") 
-   
-# single line separated by space 
-enter_values = list(map(float, input().split(","))) # Logic - seperates the float values by taking ',' as a reference, and the map operator accepts values until all rows and columns are maxed out
-  
-# For printing the matrix 
-M = Math.matrix(enter_values).reshape(R, C)  # "Reshapes" it into a matrix
-print("This is your original Matrix")
-print(M)
 
-command = input("Do you wish to do basicops, complexops, or exit? ") # Continue or exit dialog
+# Methods end
+
+command = input("Do you wish to do basicops, complexops, or exit? ") # Actual Program (Main) starts from here
 if(command == "exit"):
     input.close()
     
@@ -56,7 +75,7 @@ elif(command == "basicops"):
     
 elif(command == "complexops"):
     LinearAlg()
-
+    
 
 
 
