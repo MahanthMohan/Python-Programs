8cb61b12ec0c1dcdb3f6faa89cde053ea71a9888
Mahanth <mohan.mahanth@gmail.com>
2020-03-14 13:30:00 -0700

    Update MathAPI.py

--- SolveMatrix.py
+++ SolveMatrix.py
@@ -13,11 +13,11 @@ A = np.matrix(enter_values).reshape(R, C)  # "Reshapes" it into a matrix
 print("\nThis is the A Matrix")
 print(A)
 
-print("Enter the entries in a single line for the B matrix, rowwise , and spaced out using commas ") 
+print("Enter the entries in a single line for the C matrix, rowwise , and spaced out using commas ") 
 # single line separated by space 
 enter_values = list(map(float, input().split(","))) # Logic - seperates the float values by taking ',' as a reference, and the map operator accepts values until all rows and columns are maxed out
 # For printing the matrix 
-C = np.matrix(enter_values).reshape(R, C)  # "Reshapes" it into a matrix
+C = np.matrix(enter_values).reshape(2, 1)  # "Reshapes" it into a matrix
 print("\nThis is the C Matrix")
 print(C)
 
