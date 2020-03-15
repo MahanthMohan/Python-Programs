fd41a427150ff5077ab0b664b54382f71d6decb3
Mahanth <mohan.mahanth@gmail.com>
2020-03-14 20:04:20 -0700

    Update AudioPlayer.py

--- AudioPlayer.py
+++ AudioPlayer.py
@@ -1,11 +1,13 @@
 import os
 import simpleaudio as audio
 from playsound import playsound
+
+print("*** A simple audio player ***")
 audio = input("Enter the name of the audio file: ")
 format = input("Enter the audio format (mp3 or wav) : ")
 
 if(format == "mp3"):
-     playsound(audio + ".{}".format(format))
+ playsound(audio + ".{}".format(format))
 elif(format == "wav"):
  audio_request = audio.WaveObject.from_wave_file(audio + ".{}".format(format))
  play = audio_request.play()
