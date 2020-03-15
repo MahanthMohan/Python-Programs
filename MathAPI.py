a9559fbc4f791efd331d1c3cb96d13311d160bf2
Mahanth <mohan.mahanth@gmail.com>
2020-03-14 18:01:33 -0700

    Update NewtonAPI.py

--- MathAPI.py
+++
@@ -1,8 +0,0 @@
-import requests
-operation = input("Enter any operation ")
-expression = input("Enter the expression ")
-API_URL = "https://newton.now.sh/operation/expression"
-request_URL = "https://newton.now.sh/" + operation + "/" + expression
-return_data = requests.get(request_URL).json()
-result = return_data['result']
-print(result)
---
+++ NewtonAPI.py
@@ -0,0 +1,11 @@
+import requests
+common_operations = "operations: simplify, factor, derive, integrate, zeroes, tangent, area, cosin, sin, tan, arcos, arcsin, arctan, abs, log"
+expression = "All entered expressions should be algebraic"
+print("\n" + common_operations + "\n" + expression)
+operation = input("Enter any operation: ")
+expression = input("Enter the expression: ")
+API_URL = "https://newton.now.sh/operation/expression"
+request_URL = "https://newton.now.sh/" + operation + "/" + expression
+return_data = requests.get(request_URL).json()
+result = return_data['result']
+print(result) 
