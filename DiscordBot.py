32d1f570b99808eeef22c0dae3614daae5f41c2c
Mahanth <mohan.mahanth@gmail.com>
2020-03-15 15:50:33 -0700

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,11 +1,11 @@
 import discord
 
-token = "Njg4ODI4NjY2NTQ4OTc3NzM3.Xm6AJA.VevX0peWp1sLXOppInV0EsFGFcg"
+token = "Njg4ODI4NjY2NTQ4OTc3NzM3.Xm6w-w.f7JGqmKIQP8rusai5CWuoxeXT3g"
 client = discord.Client()
 
 @client.event
 async def on_message(message):
-    if message.content.find("!hello") != -1:
-     await message.channel.send("Hello!")
+ if message.content.find("!hello") != -1:
+      await message.channel.send("Hello!")
 
 client.run(token)
