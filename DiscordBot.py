6c674117f207acf58e087ecc5bbd8f7d3ee94dd9
Mahanth <mohan.mahanth@gmail.com>
2020-03-15 19:07:01 -0700

    Updates

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,6 +1,6 @@
 import discord
 
-token = "Njg4ODI4NjY2NTQ4OTc3NzM3.Xm6w-w.f7JGqmKIQP8rusai5CWuoxeXT3g"
+token = "Njg4ODI4NjY2NTQ4OTc3NzM3.Xm7TRA.QTCmbv3W1pTGi9KQOhr4xPY0tSQ"
 client = discord.Client()
 
 @client.event
--- WesternShowdown.py
+++ WesternShowdown.py
@@ -1,10 +1,11 @@
 def showdown(p1, p2):
  if p1.find("Bang!") < p2.find("Bang!"):
-  return("→ p1")
-  return("p1 draws his gun sooner than p2")
+  print("p1")
  elif p1.find("Bang!") > p2.find("Bang!"):
-  return("→ p2")
-  return("p2 draws his gun sooner than p1")
- elif p1.find("Bang!") == p2.find("Bang!"):
-  return("→ tie")
-  return("tie")
+  print("p2")
+ else:
+  print("tie")
+
+p1 = str(input("Enter Bang! to shoot for player 1: "))
+p2 = str(input("Enter Bang! to shoot for player 2: "))
+showdown(p1,p2)
