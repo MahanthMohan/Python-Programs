6152c26bd1996f3c3ca69db04c405ff2dbb931fb
Mahanth <mohan.mahanth@gmail.com>
2020-03-17 18:42:51 -0700

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,30 +1,30 @@
-import discord
-import requests 
+import discord.ext.commands 
+from discord.ext.commands import Bot
 
-token = "Discord_bot_token"
+token = 'Bot_token_here'
+
 client = discord.Client()
+bot = Bot(command_prefix='$') 
 
 @client.event
-async def bot_responses(message):
- if message.content.find("!hello") != -1:
-      await message.channel.send("Hello!")
+async def on_message(message):
 
- elif message.content.find("!How are you?") != -1:
-      await message.channel.send("Great!")
-       
- elif message.content.find("!Who are you?") != -1:
-      await message.channel.send("I am a bot, but consider me your friend!")
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hello!")
+
+    if message.content.startswith("$shrug"):
+        await message.channel.send('¯\_(ツ)_/¯')
     
- elif message.content.find("!How is the weather?") != -1:
-       API_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=a82ce5d667628af3985ec52d8a1a91eb'
-       request_url = 'http://api.openweathermap.org/data/2.5/weather?appid=a82ce5d667628af3985ec52d8a1a91eb&q={}'.format("Cupertino")
-       return_data = requests.get(request_url).json()
-       relevant_data = return_data['main']
-       description = relevant_data['description']
-       current_temp = 'and the Current temperature is ' + str(int((relevant_data['temp']) - 273)) + " degrees Celsius"
-       await message.channel.send("The weather in Cupertino is " + current_temp)
- 
- elif message.content.find("!Thank you") != -1:
-       await message.channel.send("Happy to help, as always!") 
-        
+    if message.content.find("$How are you?") != -1:
+        await message.channel.send("Great!")
+       
+    if message.content.find("$Who are you?") != -1:
+        await message.channel.send("I am a bot, but consider me your friend!")
+    
+    if message.content.find("$Thank you") != -1:
+        await message.channel.send("Happy to help, as always!") 
+
 client.run(token)
