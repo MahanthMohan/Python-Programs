1087545a20a5739a134d04b97dcf9e0d32cb0151
Mahanth <mohan.mahanth@gmail.com>
2020-03-17 22:28:42 -0700

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,7 +1,8 @@
 import discord.ext.commands 
 from discord.ext.commands import Bot
+import wikipedia
 
-token = 'Bot_token_here'
+token = 'bot_token_here'
 
 client = discord.Client()
 bot = Bot(command_prefix='$') 
@@ -26,5 +27,22 @@ async def on_message(message):
     
     if message.content.find("$Thank you") != -1:
         await message.channel.send("Happy to help, as always!") 
+        
+    if message.content.find("$What do you eat?") != -1:
+            await message.channel.send("Loads of data!")
+            
+    if message.content.find("$What do you eat?") != -1:
+            await message.channel.send("Loads of data!") 
 
+    if message.content.find("$Search") != -1:
+            await message.channel.send("Enter the topic: ")
+            search = message.content 
+            return_content = wikipedia.summary(search, sentences = 5)
+            await message.channel.send(return_content)
+            
+    if message.content.find("$users") != -1:
+            id = client.get_guild(689260912200122383)
+            await message.channel.send("# of Members are {}".format(id.member_count))
+      
+            
 client.run(token)
