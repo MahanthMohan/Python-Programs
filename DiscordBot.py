acbab0f8732d8487c8b74259b994432cb40d21e3
Mahanth <mohan.mahanth@gmail.com>
2020-03-18 18:57:43 -0700

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -10,36 +10,46 @@ bot = Bot(command_prefix='$')
 @client.event
 async def on_message(message):
 
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
+    if message.content == '$hello':
         await message.channel.send("Hello!")
 
-    if message.content.startswith("$shrug"):
+    if message.content == "$shrug":
         await message.channel.send('¯\_(ツ)_/¯')
     
-    if message.content.find("$How are you?") != -1:
+    if message.content == "$how are you?" != -1:
         await message.channel.send("Great!")
        
-    if message.content.find("$Who are you?") != -1:
+    if message.content == "$who are you?" != -1:
         await message.channel.send("I am a bot, but consider me your friend!")
     
-    if message.content.find("$Thank you") != -1:
+    if message.content == "$thank you" != -1:
         await message.channel.send("Happy to help, as always!") 
         
-    if message.content.find("$What do you eat?") != -1:
+    if message.content == "$what do you eat?" != -1:
             await message.channel.send("Loads of data!")
 
-    if message.content.find("$Search") != -1:
-            await message.channel.send("Enter the topic: ")
+    if message.content == "$search" != -1:
             search = message.content 
             return_content = wikipedia.summary(search, sentences = 5)
             await message.channel.send(return_content)
             
-    if message.content.find("$users") != -1:
+    if message.content == "$users" != -1:
             id = client.get_guild(689260912200122383)
             await message.channel.send("# of Members are {}".format(id.member_count))
-      
             
+    if message.content == "$server" != -1:
+            ch = client.get_guild(689260912200122383)
+            await message.channel.send("The server name is " + str(ch))
+ 
+    if message.content == "$channel" != -1:
+            ch = message.channel
+            await message.channel.send("This message was sent to the " + str(ch) + " channel") 
+
+    if message.content == "$names" != -1:
+            await message.channel.send(str(client.users))
+            
+    if message.content == "$terminate" != -1:
+            await message.channel.send("The bot will be terminated..")
+            client.logout()   
+                                   
 client.run(token)
