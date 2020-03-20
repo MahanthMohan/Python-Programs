4f1c1c2c31ae2356e3c9ee22a009b47ef99ba485
Mahanth <mohan.mahanth@gmail.com>
2020-03-19 21:27:23 -0700

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -2,7 +2,7 @@ import discord.ext.commands
 from discord.ext.commands import Bot
 import wikipedia
 
-token = 'Njg5MjYxMjk0NTIzNzc3MDgz.XnLt9w.MYQWqCRjE8HiddG78i23sNm5XPo'
+token = 'bot_token_here'
 
 client = discord.Client()
 bot = Bot(command_prefix='$') 
@@ -10,36 +10,36 @@ bot = Bot(command_prefix='$')
 @client.event
 async def on_message(message):
 
-    if message.content == '$hello':
+    if message.content.find('$hello') != -1:
         await message.channel.send("Hello!")
 
-    if message.content == "$shrug":
+    if message.content.find("$shrug") != -1:
         await message.channel.send('¯\_(ツ)_/¯')
     
-    if message.content == "$how are you?" != -1:
+    if message.content.find("$how are you?") != -1:
         await message.channel.send("Great!")
        
-    if message.content == "$who are you?" != -1:
+    if message.content.find("$who are you?") != -1:
         await message.channel.send("I am a bot, but consider me your friend!")
     
-    if message.content == "$thank you" != -1:
+    if message.content.find("$thank you") != -1:
         await message.channel.send("Happy to help, as always!") 
         
-    if message.content == "$what do you eat?" != -1:
+    if message.content.find("$what do you eat?") != -1:
             await message.channel.send("Loads of data!")
 
-    if message.content.startswith("$search") != -1:
+    if message.content.find("$search") != -1:
             search = message.content
             splitted_search = search.split(" ")
             query = splitted_search[1] 
             return_content = wikipedia.summary(query, sentences = 3)
             await message.channel.send(return_content)
             
-    if message.content == "$users" != -1:
+    if message.content.find("$users") != -1:
             id = client.get_guild(689260912200122383)
             await message.channel.send("# of Members are {}".format(id.member_count))
             
-    if message.content == "$server" != -1:
+    if message.content.find("$server") != -1:
             ch = client.get_guild(689260912200122383)
             await message.channel.send("The server name is " + str(ch))
  
@@ -47,10 +47,10 @@ async def on_message(message):
             ch = message.channel
             await message.channel.send("This message was sent to the " + str(ch) + " channel") 
 
-    if message.content == "$names" != -1:
+    if message.content.find("$names") != -1:
             await message.channel.send(str(client.users))
             
-    if message.content == "$author" != -1:
+    if message.content.find("$author") != -1:
             author = message.author
             await message.channel.send("the author of this message is " + str(author))
                                             
