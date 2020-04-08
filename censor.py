2a04d5c050c03a7946d827412ad6b33a1ce7ae56
Mahanth <mohan.mahanth@gmail.com>
2020-04-08 11:45:42 -0700

    Commits from Atom

--- censor.py
+++ censor.py
@@ -1,12 +1,16 @@
 def censor_string():
     censor_text = str(input("Enter the text: "))
-    censor_word = str(input("Enter the censor: "))
+    censor_word = str(input("Enter the censor(s), spaced out using comma(s): "))
     censor_character = str(input("Enter a censor character: "))
-    censor_string = (censor_text , [str(censor_word)," "], censor_character)
-    print("This is the text you entered: " + "{}".format("\n") + censor_string[0])
-    censor_length = len(censor_string[1][0])
-    final_trim = censor_length * censor_string[2]
-    censored_string = censor_string[0].replace(censor_word, final_trim)
-    print("This is the censored string: " + "{}".format("\n") + censored_string)
+    splitted_censor = censor_word.split(",")
+    censor_list_length = len(splitted_censor)
+    print("This is the text you entered: " , "\n" , censor_text)
+    for i in range (0,censor_list_length):
+        i = i
+        censor_string = (censor_text , splitted_censor[i] , censor_character)
+        trim = 0
+        trim = len(censor_string[i]) * censor_string[2]
+        censored_string = censor_string[0].replace(splitted_censor[i],trim)
+        print("This is the censored string: " , "\n" , censored_string)
 
 censor_string()
---
+++ wordspeller.py
@@ -0,0 +1,7 @@
+
+word = str(input("Enter a word to spell it out: "))
+word_length = len(word)
+
+for start_index in range(0,word_length):
+    start_index = start_index
+    print(word[start_index])
