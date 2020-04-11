fb1beeaa567fbaf0aee44428146cb7a1983e0634
Mahanth <mohan.mahanth@gmail.com>
2020-04-11 15:22:12 -0700

    initial commit

--- instabot.py
+++ instabot.py
@@ -1,47 +1,68 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver # A package for web automation
+from time import sleep # to make sure the bot "sleeps" well after a process
+from webdriver_manager.chrome import ChromeDriverManager # Manage exceptions from the Chrome Web Driver
 
-# A class for Instagram Bot code
-class instabot():
-    # Initialize self, username, and password through user credentials
-    def __init__(self, username, password):
-        self.browser = webdriver.Chrome(ChromeDriverManager().install())
-        self.username = username
-        self.password = password
-
-    def login(self):
-        self.browser.get('https://www.instagram.com/accounts/login')
-
-        emailInput = self.browser.find_elements_by_css_selector('form input')[0]
-        passwordInput = self.browser.find_elements_by_css_selector('form input')[1]
-
-        emailInput.send_keys(self.username)
-        passwordInput.send_keys(self.password)
-        passwordInput.send_keys(Keys.ENTER)
+# A class for Instagram Bot
+class Instabot:
+    def __init__(bot, username, pw): # Using bot to connect class elements to the class 
+        bot.driver = webdriver.Chrome(ChromeDriverManager().install())
+        bot.username = username
+        # Opens a new chrome window with the website url
+        bot.driver.get("https://instagram.com/accounts/login")
         sleep(2)
+        # finds the username and password elements by full xpath
+        # username
+        bot.driver.find_element_by_xpath("//input[@name=\"username\"]").send_keys(username)
+        # password
+        bot.driver.find_element_by_xpath("//input[@name=\"password\"]").send_keys(pw)
+        # submit username and password
+        bot.driver.find_element_by_xpath('/html/body/div[1]/section/main/div/article/div/div[1]/div/form/div[4]').click()
+        sleep(3)
+        # Click on 'Not Now'
+        bot.driver.find_element_by_xpath('/html/body/div[4]/div/div/div[3]/button[2]').click()
+        sleep(3)
 
-    def followUser(self, username):
-        self.browser.get('https://www.instagram.com/' + username + '/')
+    def getFollowers(bot):
+        bot.driver.find_element_by_xpath("/html/body/div[1]/section/nav/div[2]/div/div/div[3]/div/div[5]/span/img").click()
         sleep(2)
-        followButton = self.browser.find_element_by_css_selector('button')
-        if followButton.text != 'Following':
-            followButton.click()
-            sleep(2)
-        else:
-            print("You are already following this user")
-
-    def unfollowuser(self, username):
-        self.browser.get('https://www.instagram.com/' + username + '/')
+        follower_box = bot.driver.find_element_by_xpath("//a[contains(@href, '/{}/{}/')]".format(bot.username,"followers"))
+        follower_box.click()
         sleep(2)
-        followButton = self.browser.find_element_by_css_selector('button')
-        if followButton.text != 'Following':
-            followButton.click()
-            sleep(2)
-            confirmButton = self.browser.find_element_by_xpath('//button[text() = "Unfollow"]')
-            confirmButton.click()
-        else:
-            print("You are not following this user")
+        #bot.driver.execute_script("{}.scrollTo(0,{}.scrollHeight)".format(follower_box,follower_box))
+        print("You have " + follower_box.text)
 
-instabot = instabot(username, password)
-instabot.login()
-instabot.followuser()
+    
+    def getFollowing(bot):
+        bot.driver.find_element_by_xpath("/html/body/div[1]/section/nav/div[2]/div/div/div[3]/div/div[5]/span/img").click()
+        sleep(2)
+        following_box = bot.driver.find_element_by_xpath("//a[contains(@href, '/{}/{}/')]".format(bot.username,"following"))
+        following_box.click()
+        sleep(2)
+        #bot.driver.execute_script("{}.scrollTo(0,{}.scrollHeight)".format(following_box,following_box))
+        print("You have " + following_box.text + " you")
+
+    def ViewFeed(bot):
+        bot.driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+
+    def ViewPosts(bot):
+        bot.driver.find_element_by_xpath("/html/body/div[1]/section/nav/div[2]/div/div/div[3]/div/div[5]/span/img").click()
+        sleep(2)
+        bot.driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+
+username = input("Enter your username: ")
+pw = input("Enter your password: ")
+bot = Instabot(username, pw)
+command = input("What do you want to do - get followers, get following, view feed, view posts?: " + "\n")
+
+if (command == "get followers"):
+    bot.getFollowers()
+
+elif (command == "get following"):
+    bot.getFollowing()
+
+elif (command == "view feed"):
+    bot.ViewFeed()
+
+elif (command == "view posts"):
+    bot.ViewPosts()
+
