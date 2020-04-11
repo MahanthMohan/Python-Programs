d3bd8fa5d149d00a16bc423b26bab2119b06ab7d
Mahanth <mohan.mahanth@gmail.com>
2020-04-10 19:15:17 -0700

    more fixes

--- censor.py
+++ censor.py
@@ -1,16 +1,12 @@
 def censor_string():
     censor_text = str(input("Enter the text: "))
-    censor_word = str(input("Enter the censor(s), spaced out using comma(s): "))
-    censor_character = str(input("Enter a censor character: "))
-    splitted_censor = censor_word.split(",")
-    censor_list_length = len(splitted_censor)
+    censor_word = str(input("Enter the censor word: "))
+    censor_character = str(input("Enter the censor character: "))
+    censor_list_length = len(censor_word)
     print("This is the text you entered: " , "\n" , censor_text)
-    for i in range (0,censor_list_length):
-        i = i
-        censor_string = (censor_text , splitted_censor[i] , censor_character)
-        trim = 0
-        trim = len(censor_string[i]) * censor_string[2]
-        censored_string = censor_string[0].replace(splitted_censor[i],trim)
-        print("This is the censored string: " , "\n" , censored_string)
+    censor_string = (censor_text , censor_word , censor_character)
+    trim = len(censor_string[1]) * censor_string[2]
+    censored_string = censor_string[0].replace(censor_word,trim)
+    print("This is the censored string: " , "\n" , censored_string)
 
 censor_string()
--- wordspeller.py
+++ wordspeller.py
@@ -1,7 +1,17 @@
-
+from gtts import gTTS
+from playsound import playsound as ps
 word = str(input("Enter a word to spell it out: "))
 word_length = len(word)
 
-for start_index in range(0,word_length):
-    start_index = start_index
-    print(word[start_index])
+command = str(input("Enter a command (spell or speak): "))
+
+if command == "spell":
+    for start_index in range(0,word_length):
+        start_index = start_index
+        print(word[start_index], end = " ")
+
+elif command == "speak":
+    tts = gTTS(word)
+    tts.save('tts.mp3')
+    ps("tts.mp3")
+
