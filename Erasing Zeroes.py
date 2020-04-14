8f704e77e79f9aea90f045c185c7443c163ad70d
Mahanth <mohan.mahanth@gmail.com>
2020-04-13 17:09:17 -0700

    commit

---
+++ Erasing Zeroes.py
@@ -0,0 +1,5 @@
+# A program to group all 1's and 0's zeroes separately
+
+input = input("Enter the values (sequences of 0 or 1): ")
+print("These are the values you entered: {}{}".format("/n", input))
+print(input.find("1"))
