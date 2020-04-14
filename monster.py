9254e85ce406291fd9b88b17c1002341c169e12e
Mahanth <mohan.mahanth@gmail.com>
2020-04-14 13:48:50 -0700

    commit

--- monster.py
+++ monster.py
@@ -5,11 +5,12 @@ class monster:
     def eliminate(self,n):
         self.n = n
         for i in range(0, self.n):
-            if(i%2 != 0):
+            if(n == 0):
+                return 0
+            elif(n > 0 and i%2 != 0):
                 initial_values = np.array([np.arange(1,self.n + 1)])
                 output = np.array([initial_values[0][i]])
-                print(output)
 
 monster = monster()
-monster.eliminate(1000)      
+print()
 
