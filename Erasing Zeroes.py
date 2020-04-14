f4d848caeeb7c99ad5cdf46a9a2d19701b125b98
Mahanth <mohan.mahanth@gmail.com>
2020-04-13 17:51:30 -0700

    commit Erasing Zeroes.py

--- Erasing Zeroes.py
+++ Erasing Zeroes.py
@@ -1,5 +1,10 @@
 # A program to group all 1's and 0's zeroes separately
+# Also, calculates minimum number of 0's to be eliminated 
 
-input = input("Enter the values (sequences of 0 or 1): ")
-print("These are the values you entered: {}{}".format("/n", input))
-print(input.find("1"))
+n = int(input("Enter the number of 100's to be printed: "))
+input_values = "100" * n
+print("These are the input values: " + input_values)
+filtered_values = input_values.replace("0","*",-1)
+print("These are the filtered values: " + str(filtered_values))
+occurences = input_values.count("0",0,3)
+print("The minimum number of zero(es) that need to be erased is " + str(occurences))
