898b7c2a6899bb5f775813ca6317a12b939de99d
Mahanth <mohan.mahanth@gmail.com>
2020-04-14 11:58:40 -0700

    updates

--- monster.py
+++ monster.py
@@ -4,12 +4,10 @@ class monster:
 
     def eliminate(self,n):
         self.n = n
-        for i in range(0, self.n):
-            if(i%2 == 0):
-                i = i
-                initial_values = np.array([np.arange(1,n)])
-                rem = initial_values[0][i]
-                print(rem)
+        for i in range(0, self.n - 1):
+            if(i%2 != 0):
+                initial_values = np.array([np.arange(1,self.n)])
+                out = np.matrix([initial_values[0][i]]).reshape(1,int(self.n/2))
 
 monster = monster()
 monster.eliminate(1000)      
