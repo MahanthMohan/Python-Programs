c19d3e9a37e9bd6117857525a57511a7aeba91de
Mahanth Mohan <mohan.mahanth@gmail.com>
2020-05-20 12:24:12 -0700

    fixes

---
+++ DiscordBot/voicebot.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+
+def get_token():
+    f = open("token.txt", "r")
+    token = f.read().replace("bot_token: ","")
+    return token
+
+bot = commands.Bot(command_prefix = '$')
+
+@bot.command()
+async def join(ctx):
+    channel = ctx.author.voice.channel
+    await channel.connect()
+@bot.command()
+async def leave(ctx):
+    await ctx.voice_client.disconnect()
+
+bot.run(get_token())
