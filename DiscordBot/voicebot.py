d25f7258903a947bdcf7817bdf9785fa350c4478
Mahanth Mohan <mohan.mahanth@gmail.com>
2020-05-20 13:13:01 -0700

    commit

--- DiscordBot/voicebot.py
+++ DiscordBot/voicebot.py
@@ -12,6 +12,7 @@ bot = commands.Bot(command_prefix = '$')
 async def join(ctx):
     channel = ctx.author.voice.channel
     await channel.connect()
+    
 @bot.command()
 async def leave(ctx):
     await ctx.voice_client.disconnect()
