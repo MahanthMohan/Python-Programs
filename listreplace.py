b60eb100960c00adb650e9c850b8da16d0913bb7
Mahanth <mahanth@localhost.localdomain>
2020-05-29 01:56:45 -0300

    fixes from linux

--- listreplace.py
+++ listreplace.py
@@ -1,20 +1,31 @@
 
 class listreplace:
 
-    def replace(self):
-        for i in range(10000):
-            ret = ""
-            if i%3 == 0:
-                ret = ret + "fizz"
-            if i%5 == 0:
-                ret = ret + "buzz"
-            if ret == "":
-                ret = str(i)
-            print(ret)
+    def replace(self,list):
+	self.list = list
+	replace_list = ["fizz","buzz","fizzbuzz"]
+        for index in range(10000):
+		if(list[index]%3 == 0):
+			list[index] = replace_list[1]
+                if(list[index]%5 == 0):
+                        list[index] = replace_list[2]
+                if(list[index]%3 == 0 and list[index]%5 == 0):
+                        list[index] = replace_list[3] 
+	return list 
+
+   def create_list(self, max):
+	self.max = max
+	i = 0
+	lst = []
+	while(i <= max):
+		i = i + 1
+		lst.append(i)
+	return lst
+
 
 listreplace = listreplace()
 
-print(listreplace.replace())
+print(listreplace.replace(listreplace.create_list(10000)))
 
 
 
