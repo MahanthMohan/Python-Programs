7b26d6f7cc2aba8bb6931e3873fb33a08bc85ff7
Mahanth <mohan.mahanth@gmail.com>
2020-06-09 14:44:38 -0700

    push changes

--- weather.py
+++ weather.py
@@ -3,44 +3,41 @@ import tkinter as tk
 
 root = tk.Tk()
 
-# A method that places the labels onto the canvas
-def DisplayWeather():
-    CL.place(x = '240', y = '125')
-    WL1.place(x = '240', y = '170')
-    WL2.place(x = '240', y = '205')
-
 # A method that collects the user location from the GUI
 def getLocation():
-   City = tk.Label(root, text="City")
-   City.place(x = '240', y = '90')
+   MainLabel = tk.Label(root, text = "Get Weather Data")
+   MainLabel.place(x = '281.25', y = '50')
+   City = tk.Label(root, text = "Your City/Location:")
+   City.place(x = '281.25', y = '100')
    e = tk.Entry(root)
-   location = str(e.get())
+   Canvas.create_window('332.5', '135', window = e)
+   location = e.get()
    return location
 
 # Creates a canvas
-Canvas = tk.Canvas(root, height = '270', width = '480')
+Canvas = tk.Canvas(root, height = '562.5', width = '750', relief = 'raised')
 Canvas.pack()
 
-# A button to display weather, which is done by calling the command DisplayWeather
-WeatherButton = tk.Button(root, text = "Display Weather", command = DisplayWeather)
-WeatherButton.pack()
-WeatherButton.place(x = '240', y = '255', height = '55', width = '125')
-
-city = getLocation()
+getLocation()
+city = "Sunnyvale"
 request_url = 'http://api.openweathermap.org/data/2.5/weather?appid=a82ce5d667628af3985ec52d8a1a91eb&q={}'.format(city)
-# This is the link that has two attributes: Your api id and the location request
 
-# Displays the json weather data in form of an integer
+WeatherButton = tk.Button(root, text = "Display Weather")
+WeatherButton.pack()
+WeatherButton.place(x = '270', y = '170', height = '45', width = '125')
+
 return_data = requests.get(request_url).json()
 temp_data = return_data['main']['temp']
-weather_data = return_data['weather']
-Current_temp = 'The Current temperature is ' + str(int(temp_data - 273)) + " degrees Celsius"
+temp = 'The temperature is ' + str(int(temp_data - 273)) + " degrees Celsius"
+weather_data = str(return_data['weather'])
+description = weather_data.split(',')
 
-CL = tk.Label(root, text = "Today's weather for the city of {}: ".format(city))
-WL1 = tk.Label(root, text = Current_temp)
-WL2 = tk.Label(root, text = temp_data)
-WL3 = tk.Label(root, text = weather_data)
-# Labels that display the parsed json text
+CL = tk.Label(root, text = "Weather in {}: ".format(city))
+WL1 = tk.Label(root, text = description)
+WL2 = tk.Label(root, text = "--> " + temp)
+CL.place(x = '220', y = '270')
+WL1.place(x = '230', y = '320')
+WL2.place(x = '230', y = '350')
 
 tk.mainloop()
 # Closes the execution of tkinter
